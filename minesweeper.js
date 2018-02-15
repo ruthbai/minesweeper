f8ddb5f77852ef4943a06311e67025d0ac7d90a6
Ruthbai <minjoobai@gmail.com>
2018-02-15 14:40:55 +1300

    make board appear

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-// var board = 
+
+var board = {}
+  board.cells = [{row: 0, col: 0, isMine: false, hidden: true}, 
+                 {row: 0, col: 1, isMine: true, hidden: true}, 
+                 {row: 0, col: 2, isMine: false, hidden: true}, 
+                 {row: 1, col: 0, isMine: true, hidden: true},
+                 {row: 1, col: 1, isMine: false, hidden: true},
+                 {row: 1, col: 2, isMine: true, hidden: true},
+                 {row: 2, col: 0, isMine: false, hidden: true},
+                 {row: 2, col: 1, isMine: false, hidden: true},
+                 {row: 2, col: 2, isMine: true, hidden: true}]
+
+
+
+  // This next line will just add it to the <body> tag
+  document.body.appendChild(img);
+
 
 function startGame () {
   // Don't remove this function call: it makes the game work!
