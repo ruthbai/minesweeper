8565c9a1611931f85426f35e5764cd9ddfd8c603
Ruthbai <minjoobai@gmail.com>
2018-02-22 21:25:30 +1300

    added count

--- minesweeper.js
+++ minesweeper.js
@@ -1,25 +1,21 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-
 var board = {}
-  board.cells = [{row: 0, col: 0, isMine: false, hidden: true}, 
-                 {row: 0, col: 1, isMine: true, hidden: true}, 
-                 {row: 0, col: 2, isMine: false, hidden: true}, 
-                 {row: 1, col: 0, isMine: true, hidden: true},
-                 {row: 1, col: 1, isMine: false, hidden: true},
-                 {row: 1, col: 2, isMine: true, hidden: true},
-                 {row: 2, col: 0, isMine: false, hidden: true},
-                 {row: 2, col: 1, isMine: false, hidden: true},
-                 {row: 2, col: 2, isMine: true, hidden: true}]
-
-
-
-  // This next line will just add it to the <body> tag
-  document.body.appendChild(img);
-
+board.cells = [{row:0, col: 0, isMine: false, hidden: true},
+               {row:0, col: 1, isMine: true, hidden: true},
+               {row:0, col: 2, isMine: false, hidden: true},
+               {row:1, col: 0, isMine: true, hidden: true},
+               {row:1, col: 1, isMine: false, hidden: true},
+               {row:1, col: 2, isMine: true, hidden: true},
+               {row:2, col: 0, isMine: false, hidden: true},
+               {row:2, col: 1, isMine: false, hidden: true},
+               {row:2, col: 2, isMine: true, hidden: true}]
 
 function startGame () {
+      for (var i = 0; i < board.cells.length; i++) {
+            board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
+      }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -44,5 +40,14 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
+      var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);
+
+      var count = 0
+      for (j = 0; j < surroundingCells.length; j++) {
+            if (surroundingCells[j].isMine === true) {
+                  count++
+            }
+      }
+      return count
 }
 
